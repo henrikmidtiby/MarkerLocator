ba41b189c1231c40dd2b0ceaf5e2a33d0d6eb7b3
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2014-05-21 10:37:40 +0200

    Datatype.

---
+++ MarkerPose.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 13 19:38:43 2014
+
+@author: henrik
+"""
+
+class MarkerPose:
+    def __init__(self, x, y, theta, quality, order = None):
+        self.x = x
+        self.y = y
+        self.theta = theta
+        self.quality = quality
+        self.order = order
