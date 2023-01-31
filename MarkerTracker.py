98a00df8b748892d5ebbe736374bfbfd50594371
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2023-01-31 11:07:02 +0100

    Added code for gaining sub pixel accuracy of the location of the detected markers.

--- MarkerTracker.py
+++ MarkerTracker.py
@@ -59,6 +59,38 @@ class MarkerTracker:
 
         return np.real(kernel), np.imag(kernel)
 
+    def refine_marker_location(self):
+        try: 
+            delta = 1
+            # Fit a parabola to the frame_sum_squared marker response
+            # and then locate the top of the parabola.
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(self.frame_sum_squared)
+            x = max_loc[1]
+            y = max_loc[0]
+            frame_sum_squared_cutout = self.frame_sum_squared[x-delta:x+delta+1, y-delta:y+delta+1]
+            # Taking the square root of the frame_sum_squared improves the accuracy of the 
+            # refied marker position.
+            frame_sum_squared_cutout = np.sqrt(frame_sum_squared_cutout)
+
+            nx, ny = (1 + 2*delta, 1 + 2*delta)
+            x = np.linspace(-delta, delta, nx)
+            y = np.linspace(-delta, delta, ny)
+            xv, yv = np.meshgrid(x, y)
+
+            xv = xv.ravel()
+            yv = yv.ravel()
+
+            coefficients = np.concatenate([[xv**2], [xv], [yv**2], [yv], [yv**0]], axis = 0).transpose()
+            values = frame_sum_squared_cutout.ravel().reshape(-1, 1)
+            solution, residuals, rank, s = np.linalg.lstsq(coefficients, values, rcond=None)
+            dx = -solution[1] / (2*solution[0])
+            dy = -solution[3] / (2*solution[2])
+            return dx[0], dy[0]
+        except np.linalg.LinAlgError:
+            # This error is triggered when the marker is detected close to an edge.
+            # In that case the refine method bails out and returns two zeros.
+            return 0, 0
+
     def locate_marker(self, frame):
         assert len(frame.shape) == 2, "Input image is not a single channel image."
         self.frame_real = frame.copy()
@@ -75,6 +107,9 @@ class MarkerTracker:
         self.last_marker_location = max_loc
         self.determine_marker_orientation(frame)
         self.determine_marker_quality(frame)
+        dx, dy = self.refine_marker_location()
+        #print(f"dx: {dx: 0.2f}  dy: {dy: 0.2f}")
+        max_loc = (max_loc[0] + dx, max_loc[1] + dy)
 
         self.pose = MarkerPose(max_loc[0], max_loc[1], self.orientation, self.quality, self.order)
         return self.pose
